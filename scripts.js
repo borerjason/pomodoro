0c0e923b3471871a8a7dfea7cbd5682911cba871
Jason Borer <borerjason@gmail.com>
2017-07-31 22:48:04 -0700

    updated links

--- scripts.js
+++ scripts.js
@@ -1,9 +1,16 @@
 $(document).ready(function(){
 
 
-	$(.subtract).onclick(function(){
+	$("#minusBreak").click(subtract);
 
-	});
 
 
 });
+
+function subtract(){
+	// take the number value in html and subtract one 
+	// if I click the ID #minusBreak then the class .breakTime should go down by one
+	var $jqbreakTime = #('.breakTime')
+	$jqbreakTime --;
+	$('breakTime').html($jqbreakTime);
+};
